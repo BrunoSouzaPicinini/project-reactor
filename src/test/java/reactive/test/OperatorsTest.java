410d1a96a2e8b6dcbffa05cd7d00b42ee00d9ab8
Bruno de Souza Picinini <brunosouzapicinini@gmail.com>
2022-02-10 23:16:22 -0300

    PublishOn and SubscribeOn example

---
+++ src/test/java/reactive/test/OperatorsTest.java
@@ -0,0 +1,130 @@
+package reactive.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
+
+@Slf4j
+public class OperatorsTest {
+
+    @Test
+    public void subscribeOnSimple() {
+        Flux<Integer> flux = Flux.range(1, 4)
+                .map(i -> {
+                    log.info("Map 1 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                })
+                .subscribeOn(Schedulers.boundedElastic()) //Effects all in chain, all chain will use this thread scheduler
+                .map(i -> {
+                    log.info("Map 2 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                });
+
+        StepVerifier.create(flux)
+                .expectSubscription()
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+
+    @Test
+    public void publishOnSimple() {
+        Flux<Integer> flux = Flux.range(1, 4)
+                .map(i -> {
+                    log.info("Map 1 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                })
+                .publishOn(Schedulers.boundedElastic()) //Effects only nodes after this declaration, they will be in another thread
+                .map(i -> {
+                    log.info("Map 2 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                });
+
+        StepVerifier.create(flux)
+                .expectSubscription()
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+
+    @Test  //use this only if it's extremely necessary
+    public void multipleSubscribeOnSimple() {
+        Flux<Integer> flux = Flux.range(1, 4)
+                .subscribeOn(Schedulers.single()) // the first configuration will be used
+                .map(i -> {
+                    log.info("Map 1 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                })
+                .subscribeOn(Schedulers.boundedElastic())
+                .map(i -> {
+                    log.info("Map 2 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                });
+
+        StepVerifier.create(flux)
+                .expectSubscription()
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+
+    @Test
+    public void multiplePublishOnSimple() {
+        Flux<Integer> flux = Flux.range(1, 4)
+                .publishOn(Schedulers.single()) //Effects only nodes after this declaration, they will be in single thread
+                .map(i -> {
+                    log.info("Map 1 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                })
+                .publishOn(Schedulers.boundedElastic()) //Effects only nodes after this declaration, they will be in another thread
+                .map(i -> {
+                    log.info("Map 2 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                });
+
+        StepVerifier.create(flux)
+                .expectSubscription()
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+
+    @Test
+    public void publishAndSubscribeOnSimple() {
+        Flux<Integer> flux = Flux.range(1, 4)
+                .publishOn(Schedulers.single()) //It will affect everything
+                .map(i -> {
+                    log.info("Map 1 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                })
+                .subscribeOn(Schedulers.boundedElastic())//It's not affecting
+                .map(i -> {
+                    log.info("Map 2 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                });
+
+        StepVerifier.create(flux)
+                .expectSubscription()
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+
+    @Test
+    public void subscribeAndPublishOnSimple() {
+        Flux<Integer> flux = Flux.range(1, 4)
+                .subscribeOn(Schedulers.single()) //It will affect everything before a publishOn
+                .map(i -> {
+                    log.info("Map 1 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                })
+                .publishOn(Schedulers.boundedElastic())//Effects only nodes after this declaration, they will be in another thread
+                .map(i -> {
+                    log.info("Map 2 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                });
+
+        StepVerifier.create(flux)
+                .expectSubscription()
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+}
